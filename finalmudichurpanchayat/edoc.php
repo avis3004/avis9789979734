514eca0befca27880efb434f2deecd327303882a
sivachitra <sivachitravijayan@gmail.com>
2015-02-04 11:54:57 -0500

    dynamic news feed without css

--- finalmudichurpanchayat/edoc.php
+++ finalmudichurpanchayat/edoc.php
@@ -1,13 +1,47 @@
 <?php
 include 'header.php';
+include 'MudichurPanchayat/database_connection.php';
+$date= date('Y-m-d H:i:s');
+$news=$_POST['message'];
+ $query=("INSERT INTO panchayat.newsfeed (idnewsfeed, newsDate, newsContent)
+  VALUES (NULL, '$date', '$news');");
+  $rs_dnews = mysql_query($query) or die("Query to get data from news table failed: " . mysql_error());
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Untitled Document</title>
+<title>Upload form</title>
 </head>
-
 <body>
+
+<section>
+<table width="1172" border="1">
+  <tr>
+    <td height="110" colspan="2">
+	<div>
+		<center		<h1>UPLOAD YOUR FILES HERE...</h1></center>
+		
+                 </div>       </td>
+  </tr>
+  <tr>
+    <td>
+	Insert the news inside the box:
+	<form action="<?php $_PHP_SELF ?>" method="post">
+	<textarea name="message" rows="10" cols="30">
+    </textarea>
+	<input type="submit" />
+	</form></td>
+    <td>
+	<?php
+			include 'MudichurPanchayat/GalleryUpload.php';
+			?></td>
+  </tr>
+</table>
+
+</section>
+<?php
+        include 'footer.php';
+        ?>
 </body>
 </html>
