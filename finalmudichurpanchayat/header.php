f549ed35328db12182c04a3b1517a9d7a0009be3
avis3004 <sivaganeshvbe@gmail.com>
2015-02-05 00:16:58 +0530

    Revert "included header and footer in PhpGrid form"
    This reverts commit 692457944e3aa8ddebd3b6511ab4d3bf2c56a8e0.

--- finalmudichurpanchayat/header.php
+++ finalmudichurpanchayat/header.php
@@ -32,9 +32,8 @@
         
         <header class="title">
             <div id="logo">
-                <img src="images/header copy.png" width="700"height="130" /></div>
-				<div id="emblem">
-				<img src="images/emblem1.png" width="90"height="130" /></div>
+                <img src="images/header copy.png" width="700"height="130" />
+				<img src="images/emb.png" width="70"height="130" /></div>
       
       <?php if(isset( $_SESSION["username"])){ ?>
                 <div class="welcome">
